986c98c9d3d64369926bbda98d264ca4ee9ab30c
tom <tomstevelfq@gmail.com>
2019-11-03 20:13:35 +0800

    forced_transformation

--- exchange_parameter/exchange_parameter.cpp
+++ exchange_parameter/exchange_parameter.cpp
@@ -10,11 +10,18 @@ void exchange_parameter(int& a, int& b)
 	b = temp;
 }
 
+void exchange_parameter2(int& a, int& b)
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+}
+
 int main()
 {
 	int a, b;
 	cin >> a >> b;
-	exchange_parameter(a, b);
+	exchange_parameter2(a, b);
 	cout << a << endl << b << endl;
 	return 0;
 }
